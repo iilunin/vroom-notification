978f43906c8873d1f04f90571371a1ba95771936
Igor Ilunin <ilunin.igor@gmail.com>
2017-10-24 14:51:34 -0400

    Added AWS CloudWatch logging

--- aws_main.py
+++ aws_main.py
@@ -1,8 +1,12 @@
-import json
+from __future__ import print_function
 import os
+import json
 import requests
 from datetime import datetime
+
+
 import boto3
+
 
 def main(event, context):
     price = os.environ.get('price', 16000)
@@ -15,7 +19,8 @@ def main(event, context):
 
     data = None
 
-    url = 'https://invsearch.vroomapi.com/v2/inventory?brand=vroom&sort=&offset=0&limit=100&price_max={p}&miles_max={m}&year_max=all-years&year_min=all-years&mm=all-makes'.format(p=price, m=miles)
+    url = 'https://invsearch.vroomapi.com/v2/inventory?brand=vroom&sort=&offset=0&limit=100&' \
+          'price_max={p}&miles_max={m}&year_max=all-years&year_min=all-years&mm=all-makes'.format(p=price, m=miles)
 
     r = requests.get(url)
 
@@ -27,15 +32,15 @@ def main(event, context):
 
     bucket_key = list(bucket.objects.filter(Prefix=dir))
 
-    checkedVins = None
+    checked_vins = None
 
     if len(bucket_key) > 0:
         data_files = list(bucket.objects.filter(Prefix=data_file))
         if len(data_files) > 0:
-            exisiting_vins_obj = bucket.Object(data_file)
-            contents = exisiting_vins_obj.get()['Body'].read().decode('utf-8')
+            existing_vins_obj = bucket.Object(data_file)
+            contents = existing_vins_obj.get()['Body'].read().decode('utf-8')
             old_vins = json.loads(contents)
-            checkedVins = [v['attributes']['vin'] for v in old_vins]
+            checked_vins = [v['attributes']['vin'] for v in old_vins]
 
     else:
         bucket.put_object(Key=dir+'/')
@@ -43,6 +48,7 @@ def main(event, context):
     new_cars_file = os.path.join(dir, datetime.now().__format__('%Y%m%d_%H%M%S') + '.txt')
 
     new_cars = ''
+    num_new_cars = 0
     for car_item in data.get('data'):
         car = car_item.get('attributes')
 
@@ -55,12 +61,14 @@ def main(event, context):
         if car.get('transmission', '') == 'manual':
             continue
 
-        if checkedVins and car.get('vin') in checkedVins:
+        if checked_vins and car.get('vin') in checked_vins:
             continue
 
-        carline = '{make} {model} {year}. ${listingPrice}, {miles} mi. Warranty remaining:{warrantyRemaining}, VIN: {vin}, url:https://www.vroom.com/inventory/{vin} carfax:{uriCarfax}'.format(**car)
+        carline = '{make} {model} {year}. ${listingPrice}, {miles} mi. Warranty remaining:{warrantyRemaining}, ' \
+                  'VIN: {vin}, url:https://www.vroom.com/inventory/{vin} carfax:{uriCarfax}'.format(**car)
         print(carline)
         new_cars += carline+os.linesep
+        num_new_cars += 1
 
     if new_cars:
         bucket.put_object(Key=new_cars_file, Body=new_cars)
@@ -73,11 +81,19 @@ def main(event, context):
                         Subject='New Vroom Stuff',
                         Message=new_cars)
 
-
             cellphone = os.environ.get('cellphone')
 
             if cellphone:
-                sns.publish(PhoneNumber=cellphone, Subject='New Vroom Stuff', Message='Checkout email for the New Vroom Stuff')
+                sns.publish(
+                    PhoneNumber=cellphone,
+                    Subject='New Vroom Stuff',
+                    Message='Checkout email for the New Vroom Stuff')
+
+        print('Found {0} new cars'.format(num_new_cars))
+        return {'message': new_cars}
+
+    else:
+        print('No new cars available')
 
 
 if __name__ == '__main__':
